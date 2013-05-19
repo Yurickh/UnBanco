a46cea9416139d0b4f26d30aa604dc95f2297485
Yurickh <yurick.hauschild@gmail.com>
2013-05-18 23:27:30 -0300

    Corrigido TestUnit.h e main.cpp

--- TestUnit.h
+++ TestUnit.h
@@ -6,82 +6,35 @@
 template <typename infotype, typename classtype>
 class Test{
 	protected:
-		virtual static void test(infotype);
-		virtual static void setup() = 0;
-		virtual static void tearDown() = 0;
+		virtual static void test(infotype) =0;
+		static void setup()
+		{
+			object = new classtype;
+		}
+		static void tearDown()
+		{
+			delete object;
+		}
 	public:
-		static void run() = 0;
+		static void run(infotype testValue)
+		{
+			setup();
+			test(testValue);
+			tearDown();
+		}
 
 	protected:
 		static classtype *object;
-		static infotype testValue;
 }
 
-class TestUsrName:public Test{
-	private:
-		static void test(string);
-		static UsrName *usrName;
-		static string valid;
-		static string invalid;
-}
-
-class TestUsrPassword:public Test{
-	private:
-		static void test(string);
-		static UsrPassword *usrPassword;
-		static string valid;
-		static string invalid;
-}
-
-class TestUsrId{
-	private:
-		static void test(string);
-		static UsrId *usrId;
-		static int valid;
-		static int invalid;
-}
-
-class TestUsrMatric{
-	private:
-		static void test(int);
-		static UsrMatric *usrMatric;
-		static int valid;
-		static int invalid;
-}
-
-class TestUsrType{
-	private:
-		static UsrType *usrType;
-		static bool valid;
-		static bool invalid;
-}
-
-class TestAccNumber{
-	private:
-		static AccNumber *accNumber;
-		static int valid;
-		static int invalid;
-}
-
-class TestMoney{
-	private:
-		static Money *money;
-		static float valid;
-		static float invalid;
-}
-
-class TestPayCode{
-	private:
-		static PayCode *payCode;
-		static int valid;
-		static int invalid;
-}
-
-class TestPayDay{
-	private:
-		static PayDay *payDay;
-		static int valid;
-		static int invalid;
-}
+class TestUsrName	:public Test<string, UsrName>;
+class TestUsrPassword	:public Test<string, UsrPassword>;
+class TestUsrId		:public Test<int, UsrId>;
+class TestUsrMatric	:public Test<int, UsrMatric>;
+class TestUsrType	:public Test<bool, UsrType>;
+class TestAccNumber	:public Test<int, AccNumber>;
+class TestMoney		:public Test<float, Money>;
+class TestPayCode	:public Test<int, PayCode>;
+class TestPayDay	:public Test<int, PayDay>;
 
 #endif
--- main.cpp
+++ main.cpp
@@ -2,15 +2,24 @@
 
 int main ()
 {
-	TestUsrName::run();
-	TestUsrPassword::run();
-	TestUsrId::run();
-	TestUsrMatric::run();
-	TestUsrType::run();
-	TestAccNumber::run();
-	TestMoney::run();
-	TestPayCode::run();
-	TestPayDay::run();
+	TestUsrName::run("Yúrick.Hauschild C");
+	TestUsrName::run("Andrè@Accioly-Lima-abacate_azedo");
+	TestUsrPassword::run("andre0");
+	TestUsrPassword::run("yurick66'@#$%6543");
+	TestUsrId::run(500);
+	TestUsrId::run(-200);
+	TestUsrMatric::run(12345);
+	TestUsrMatric::run(987654321);
+	TestUsrType::run(NORMAL_ACC);
+	TestUsrType::run("abacate");
+	TestAccNumber::run(1234);
+	TestAccNumber::run(-12);
+	TestMoney::run(14.79);
+	TestMoney::run(1.0/0.0);
+	TestPayCode::run(200);
+	TestPayCode::run(-50);
+	TestPayDay::run(04122015);
+	TestPayDay::run(31022015);
 
 	return 0;
 }
